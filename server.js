d75f22dc0ebb9020c3b513ad9e45c94f9240630c
Adam <adamolsey>
2022-01-22 11:17:57 -0500

    checking issues with atlas to heroku

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ const app = express();
 
 app.use(logger("dev"));
 
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose DB credentials
-mongoose.connect(process.env.MONGODB_URI ,{ 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 
