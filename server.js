ba9903ab5280f79f30b50d5e6e84679843dc6479
Adam <adamolsey>
2022-01-22 14:00:37 -0500

    make it better

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false
 });
 
 
